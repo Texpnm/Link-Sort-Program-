c3b492a773eb192ea881d9fb05dda3c6de903a20
Texpnm <122228535+Texpnm@users.noreply.github.com>
2023-01-09 00:48:54 +0500

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct linklist{
+   int data;
+   struct linklist *next;
+}node;
+int l=0;
+int main(){
+   node *head=NULL,*temp,*temp1;
+   int len,choice,count=0,key;
+   do{
+      temp=(node *)malloc(sizeof(node));
+      if(temp!=NULL){
+         printf("
+enter the elements in a list : ");
+         scanf("%d",&temp->data);
+         temp->next=NULL;
+         if(head==NULL){
+            head=temp;
+         }else{
+            temp1=head;
+            while(temp1->next!=NULL){
+               temp1=temp1->next;
+            }
+            temp1->next=temp;
+         }
+      }else{
+         printf("
+Memory is full");
+      }
+      printf("
+press 1 to enter data into list: ");
+      scanf("%d",&choice);
+   }while(choice==1);
+   len=length(head);
+   printf("The list has %d no of nodes",l);
+   return 0;
+}
